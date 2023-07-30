ca187dc270b6c2d4244688d80e8fdf531a3345a8
Kikitojr <130502687+Kikitojr@users.noreply.github.com>
2023-07-30 07:21:04 -0300

    pop-up js popup.js
    so ira aparecer quando a pessoa entrar no site

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,16 @@
-// Função para exibir o pop-up
+// Função para exibir o pop-up apenas se ainda não foi exibido nesta sessão
 function exibirPopup() {
     var popup = document.getElementById("popup-nos");
-    popup.style.display = "block";
+
+    // Verificar se o pop-up já foi exibido nesta sessão (utilizando sessionStorage)
+    var popupExibido = sessionStorage.getItem("popupExibido");
+
+    if (!popupExibido) {
+        popup.style.display = "block";
+
+        // Marcar que o pop-up foi exibido nesta sessão para não mostrar novamente
+        sessionStorage.setItem("popupExibido", "true");
+    }
 }
 
 // Função para fechar o pop-up
